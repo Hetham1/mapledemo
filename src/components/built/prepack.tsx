409c23454af70e042916a9df857d508096e6b45e
AmirRnz <155143890+AmirRnz@users.noreply.github.com>
2025-03-19 17:13:15 +0330

    ?

--- src/components/built/prepack.tsx
+++ src/components/built/prepack.tsx
@@ -4,7 +4,7 @@ export default function Prepack() {
   return (
     <div
       id="pre-package"
-      className="min-h-screen w-full flex flex-col p-4 justify-center bg-hesam-white"
+      className="min-h-screen w-full flex flex-col p-4 justify-center bg-hesam-white pt-20"
     >
       <div className="text-center mb-12">
         <h1 className="text-4xl font-bold mb-4 text-amber-700">
